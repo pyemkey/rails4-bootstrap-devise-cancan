b84e42a647fa5c1dc4407a45d1ea78bf3ca9f94c
pyemkey <py.emkey@gmail.com>
2014-02-26 23:37:56 +0100

    Add display base error helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+	def display_base_errors resource
+		return '' if(resource.errors.empty?) or (resource.errors[:base].empty?)
+		messages = resource.errors[:base].map {|msg| content_tah(:p, msg)}.join
+		html = <<-HTML
+		%div{class: "alert alert-error alert-block"}
+			%button.close{type: "button" "data-dismiss": "alert"}&#215;
+			#{messages}
+		HTML
+		html.html_safe
+	end
 end
