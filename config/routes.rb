7d70d24fec52635a4ad0dd324d748d1c06ee5a19
pyemkey <py.emkey@gmail.com>
2014-02-26 23:41:38 +0100

    Configure routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails4BootstrapDeviseCancan::Application.routes.draw do
+  devise_for :users
+  resources :users
+  # authenticated :user do
+  #   root 'home#index'
+  # end
+  root 'home#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
