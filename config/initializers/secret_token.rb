57cb55c65f208942d2202ab7b09be8533e681950
pyemkey <py.emkey@gmail.com>
2014-02-26 17:46:09 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4BootstrapDeviseCancan::Application.config.secret_key_base = 'c656fa1d8af3228826650919eadd035cf2bb76f045735101ce373bbc9d7b9d74596b675565e0522005f607c5c6410565316247ebd57d84d600f50dd8b469088f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails4BootstrapDeviseCancan::Application.config.session_store :cookie_store, key: '_rails4-bootstrap-devise-cancan_session'
